4bded11cfdca859db81c20d0126c2c68f1e717d0
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-19 10:28:10 +0100

    viewchild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,15 +1,26 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `<h5>Buscar:</h5>
+  <!-- la etiqueta #txtTagImput la usa el decorador @ViewChild  para referenciar al elemento html -->
             <input type="text"
             class="form-control"
-            placeholder="Buscar gifs...">`
+            placeholder="Buscar gifs..."
+            (keyup.enter)="searchTag()"
+            #txtTagImput>`
 })
 
 export class SearchBoxComponent  {
+  // ViewChild para obtener el input de texto
+  @ViewChild('txtTagImput')
+  tagInput!: ElementRef<HTMLInputElement>
   constructor() { }
 
+  // Funcion para buscar gifs
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
 
 }
